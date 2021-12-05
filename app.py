d39247de3c9abc8d9043883196e8f6115e95d6f5
jr1os <dev.alirios@gmail.com>
2021-12-05 22:58:51 +0000

    added exercise :sparkles:

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = flask(__name__)
 @app.route("/")
 def index():
     return "hello world"
+
+@app.route("/add/<int:value1>/<int:value2>")
+def add(value1, value2):
+    return f"result = {value1} + {value2}"
